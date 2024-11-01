d32377a9198fa971d71a8a49b522507cb28814ec
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-11-01 11:02:53 -0300

    config coupon

---
+++ src/main/java/com/daniel/spring_api/domain/coupon/Coupon.java
@@ -0,0 +1,14 @@
+package com.daniel.spring_api.domain.coupon;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "coupon")
+@Getter
+@Setter
+public class Coupon {
+    
+}
