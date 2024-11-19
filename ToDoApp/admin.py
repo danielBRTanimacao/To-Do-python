3e33ca9562008a1d7c04e5b62df59448d792976d
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-11-19 18:16:43 -0300

    creating model and admin to manager Task

--- ToDoApp/admin.py
+++ ToDoApp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Task
 
 # Register your models here.
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = []
+    ordering = []
+    search_fields = []
+    list_editable = []
+    list_per_page = 10
+    list_max_show_all = 150
+    list_display_links = []
