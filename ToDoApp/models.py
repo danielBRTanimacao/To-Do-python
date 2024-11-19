400e17fda32ac6527eaf8eafc7fdd2d554758e87
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-11-19 17:49:04 -0300

    creating model to add task

--- ToDoApp/models.py
+++ ToDoApp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    status = models.CharField(max_length=100)
+    date = models.DateTimeField()
+    created = models.DateTimeField(auto_now_add=True)
