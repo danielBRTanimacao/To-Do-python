f357fe5b7b6b1ca7355430e846d43ffb3d429336
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-11-20 11:27:24 -0300

    creating first model

--- ToDoApp/admin.py
+++ ToDoApp/admin.py
@@ -4,10 +4,8 @@ from .models import Task
 # Register your models here.
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
-    list_display = []
-    ordering = []
-    search_fields = []
-    list_editable = []
+    list_display = ['id', 'user', 'title', 'status', 'created']
+    search_fields = ['id', 'user', 'title', 'created']
+    list_display_links = ['id', 'user', 'title']
     list_per_page = 10
     list_max_show_all = 150
-    list_display_links = []
