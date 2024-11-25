c6c5cb96e49628db6974b80a83b53c255338250d
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-11-25 12:40:30 -0300

    creating alert

--- ToDoApp/static/js/index.js
+++ ToDoApp/static/js/index.js
@@ -61,27 +61,3 @@ window.onload = function () {
         document.getElementById("imageDisplay").src = cachedImageUrl;
     }
 };
-
-document.addEventListener("DOMContentLoaded", function () {
-    const alertThreshold = 3;
-    const taskItems = document.querySelectorAll(".task-item");
-
-    taskItems.forEach((task) => {
-        const dueDateString = task.getAttribute("data-due-date");
-        if (!dueDateString) return;
-
-        const dueDate = new Date(dueDateString);
-        const currentDate = new Date();
-        const timeDifference = dueDate - currentDate;
-        const daysDifference = timeDifference / (1000 * 60 * 60 * 24);
-
-        if (daysDifference <= alertThreshold && daysDifference >= 0) {
-            task.classList.add("alert-task");
-
-            const alertIcon = document.createElement("span");
-            alertIcon.textContent = " !";
-            alertIcon.classList.add("alert-icon");
-            task.querySelector(".due-date").appendChild(alertIcon);
-        }
-    });
-});
