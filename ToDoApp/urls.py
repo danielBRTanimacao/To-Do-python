904018fae909472d25a006a42ceb79792ff64014
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-11-28 12:53:40 -0300

    moving delete

--- ToDoApp/urls.py
+++ ToDoApp/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('create/', views.create_view, name='create'),
     path('logout/', views.logout_view, name='logout'),
     path('task/<str:name>/<int:id>/',  views.task_view, name='task'),
-    path('task/<int:id>/',  views.delete_task, name='delete_task')
+    path('delete/task/<int:id>/',  views.delete_task, name='delete_task')
 ]
